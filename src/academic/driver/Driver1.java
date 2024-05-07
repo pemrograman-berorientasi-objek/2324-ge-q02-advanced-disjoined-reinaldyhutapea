a71eddf84da00e96e0fe001599646bd2213ec145
Reinaldy Hutapea <reinaldyhutapea@gmail.com>
2024-05-07 17:27:14 +0700

    1

--- src/academic/driver/Driver1.java
+++ src/academic/driver/Driver1.java
@@ -347,7 +347,6 @@ public class Driver1 {
                 }
              
                //cetak best-student
-                System.out.println(bestStudentId + "|" + bestGradeB + "/" +bestGradeA);
               
              
             } 
